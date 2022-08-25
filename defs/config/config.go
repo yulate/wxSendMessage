b3a244f0af80537a4f47f6b140f1bb823c079eb3
yulate <2113778814@qq.com>
2022-08-25 23:32:27 +0800

    解决天气等基础信息出现空值的问题

---
+++ defs/config/config.go
@@ -0,0 +1,20 @@
+package config
+
+var (
+	AppId     = ""
+	AppSecret = ""
+
+	// 天行API
+	TianApi = ""
+
+	// 发送用户ID
+	ToUserId = ""
+
+	// 发送模板ID
+	TemplateId = ""
+
+	// 在一起的日期
+	LoveTime = ""
+
+	City = ""
+)
